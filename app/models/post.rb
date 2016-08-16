ba78fae7edbaf6cbc58628b95f565287001891f5
TRINADH1! <reddy.trinadhm50@gmail.com>
2016-08-16 17:48:16 +0530

    complete app

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+	has_many :comments
+
+	has_attached_file :image, styles: { medium: "300x300#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  def self.search(search)
+
+	where("title LIKE ?","%#{search}%")
+end
+
+end
